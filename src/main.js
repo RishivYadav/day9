0fbcbe36c7cfe13885efe2af677d86af768b1edb
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-12 23:07:19 +0530

    internal module asynch readfile

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-async function sum(n1, n2) {
-    // ...
-    return n1 + n2;
-  }
-  
-  async function main() {
-    let result = await sum(1, 1);
-    console.log(result);
-  }
-  
-  main();
+import { readFile } from "node:fs/promises";
+
+async function main() {
+  let filePath = "C:/Users/91883/OneDrive/Desktop/Day9/package.json";
+  let fileData = await readFile(filePath, { encoding: "utf-8" });
+  console.log(fileData);
+}
+
+main();
