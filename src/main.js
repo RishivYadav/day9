c2b05f6457f456eabc660a4fc6fde590e76e160a
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-12 23:15:57 +0530

    internal module writefile

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { readFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
 
 async function main() {
-  let filePath = "C:/Users/91883/OneDrive/Desktop/Day9/package.json";
-  let fileData = await readFile(filePath, { encoding: "utf-8" });
-  console.log(fileData);
+  let filePath = "C:/Users/91833/OneDrive/Desktop/Day9/output1.txt";
+  let fileData = `Asynchronously writes data to a file, 
+                replacing the file if it already exists. 
+                data can be a string, a buffer, an <AsyncIterable>, 
+                or an <Iterable> object.`;
+  await writeFile(filePath, fileData);
+  console.log("Write Success");
 }
 
 main();
